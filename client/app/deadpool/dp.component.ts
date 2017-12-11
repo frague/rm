66b7d046d08b58e6562e63c2b10eae0d54ca557a
Nick Bogdanov <nbogdanov@griddynamics.com>
2017-12-11 14:20:17 +0100

    Makes dp groupped by date

--- client/app/deadpool/dp.component.ts
+++ client/app/deadpool/dp.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BaseComponent } from '../base.component';
 import { DpService } from '../services/dp.service';
+import { PrintableDatePipe } from '../pipes';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'deadpool',
@@ -10,11 +12,32 @@ import { DpService } from '../services/dp.service';
 export class DpComponent extends BaseComponent {
 
   public form = new FormGroup({});
-  items = [];
+  dates = [];
 
-  constructor(private dp: DpService) {
+  constructor(
+    private makeDate: PrintableDatePipe,
+    private dp: DpService
+  ) {
     super(dp);
   }
+
+  getAll(criteria?: any): Subscription {
+    return this.dp.getAll(criteria).subscribe(
+      data => {
+        this.dates = data.reduce((result, item) => {
+          let key = this.makeDate.transform(item.date);
+          if (!result[key]) {
+            result[key] = [];
+          }
+          result[key].push(item);
+          return result;
+        }, {});
+      },
+      error => console.log(error)
+    );
+  }
+
+
 
   hasDiff(diff: any) {
     return diff.diff !== 1 && diff.diff !== -1;
