192ab975823f81e9d192fd659611087afd1d6c27
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-01-24 16:52:04 +0400

    Adds technologies to skills list. Hides empty categories

--- client/app/planner/comments.component.ts
+++ client/app/planner/comments.component.ts
@@ -69,14 +69,36 @@ export class CommentsComponent extends BaseComponent {
     }
   }
 
+  _countSkillsDefined(skills: any) {
+    let skillsDefined = 0;
+    if (skills.categories && skills.categories.length) {
+      skills.categories.forEach(category =>
+        skillsDefined += this._countSkillsDefined(category)
+      );
+    };
+    if (skills.skills && skills.skills.length) {
+      skillsDefined += skills.skills.reduce((result, skill) => {
+        if (skill.declared_level || skill.approved_level) result += 1;
+        if (skill.techs) {
+          let techs = skill.techs.filter(tech => tech.declared_level || tech.approved_level).length;
+          if (techs && !skill.declared_level && !skill.approved_level) skill.declared_level = '-';
+          skill.techsDefined = techs;
+          result += techs;
+        }
+        return result;
+      }, 0);
+    }
+    skills.skillsDefined = skillsDefined;
+    return skillsDefined;
+  }
+
   fetchSkills() {
-    console.log(this.person);
     this.skillsService.get(this.person.login).subscribe(skills => {
+      this._countSkillsDefined(skills);
       this.skills = skills;
     });
     this.skillsService.getInfo(this.person.login).subscribe(info => {
       this.skillsInfo = info;
-      console.log(info);
     });
   }
 
--- client/app/planner/skillset.component.ts
+++ client/app/planner/skillset.component.ts
@@ -6,4 +6,5 @@ import { Component, Input } from '@angular/core';
 })
 export class SkillsetComponent {
   @Input() skills: any = null;
+  @Input() class: string = '';
 }
