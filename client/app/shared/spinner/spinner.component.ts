08b5fa47fccb316d017c30826d7c7b81a3c64416
Nikolay Bogdanov <nbogdanov@mirantis.com>
2018-03-23 22:28:21 +0400

    Adds spinner draft component

---
+++ client/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'spinner',
+  templateUrl: './spinner.component.html'
+})
+export class SpinnerComponent {
+  @Input() isShown = false;
+}
