69c5b43ae94fcde3b2c1443b2fb88f12b7df7498
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-07-23 17:55:28 +0400

    Adds avatar validation logic

--- client/app/people/avatar.component.ts
+++ client/app/people/avatar.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 
+const loginValidator = new RegExp(/^[a-z]+$/);
+
 @Component({
   selector: 'avatar',
-  templateUrl: './avatar.component.html'
+  templateUrl: './avatar.component.html',
 })
 export class AvatarComponent {
   @Input() login: string;
   @Input() useDefault = false;
 
   getStyle() {
-    let avatar = this.useDefault ? '' : 'url("https://in.griddynamics.net/service/photos/' + this.login + '.jpg"), ';
+    let avatar = (!!this.useDefault || typeof this.login === 'undefined' || !loginValidator.test(this.login)) ?
+    '' : 'url("https://in.griddynamics.net/service/photos/' + this.login + '.jpg"), ';
+
     return {
       backgroundImage: avatar + 'url("/assets/nophoto.png")'
     };
