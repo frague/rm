fcd2cc53e85104ddc4ae55cac8bc1a8416c07cc1
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-09-17 12:23:22 +0400

    Makes filters sorted by title

--- server/controllers/filter.ts
+++ server/controllers/filter.ts
@@ -3,4 +3,16 @@ import BaseCtrl from './base';
 
 export default class ResourceCtrl extends BaseCtrl {
   model = Filter;
+
+// Get all
+  getAll = (req, res) => {
+    let query = this.reduceQuery(req.query);
+    console.log('Finding all', query);
+    this.model.find(query).sort({title: 1}).limit(100).exec((err, docs) => {
+      if (err) {
+        return this._respondWithError(res, err);
+      }
+      res.json(docs);
+    });
+  }
 }
