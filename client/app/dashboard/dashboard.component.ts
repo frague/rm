295fc82442011acd2052564c64f8c95c7b3679fc
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-12-24 10:45:54 +0400

    Makes dashboard page utilize new editing component

--- client/app/dashboard/dashboard.component.ts
+++ client/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { CommentsTabComponent } from '../modal/tabs/comments-tab.component';
 import { PrintableDatePipe } from '../pipes';
 import { CommentService} from '../services/comments.service';
 import { AuthService } from '../services/auth.service';
+import { BusService } from '../services/bus.service';
 
 @Component({
   selector: 'dashboard',
@@ -12,10 +13,11 @@ export class DashboardComponent extends CommentsTabComponent {
   constructor(
     makeDate: PrintableDatePipe,
     commentService: CommentService,
+    bus: BusService,
     cd: ChangeDetectorRef,
     private auth: AuthService
   ) {
-    super(makeDate, commentService, cd);
+    super(makeDate, commentService, bus, cd);
     this.key = 'notes_' + this.auth.currentUser.username;
   }
 }
