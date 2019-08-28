aa7d4ca74a82faf81fe8ad0f8474155f0e19a08e
Nick Bogdanov <nbogdanov@griddynamics.com>
2019-08-28 13:12:14 +0400

    Fixes bamboo vacaions sync

--- server/controllers/integrations/bamboo.ts
+++ server/controllers/integrations/bamboo.ts
@@ -5,9 +5,10 @@ const env = process.env;
 const bamboo = 'api.bamboohr.com/api/gateway.php/griddynamics/v1/';
 
 export default class BambooIntegrationsCtrl {
-  private _makeRequest(endpoint: string) {
+  private _makeRequest(endpoint: string, params: string = '') {
+    console.log('https://' + env.BAMBOO_KEY + ':x@' + bamboo + endpoint + (params ? `?${params}` : ''));
     return {
-      url: 'https://' + env.BAMBOO_KEY + ':x@' + bamboo + endpoint
+      url: 'https://' + env.BAMBOO_KEY + ':x@' + bamboo + endpoint + (params ? `?${params}` : '')
     };
   }
 
@@ -21,10 +22,19 @@ export default class BambooIntegrationsCtrl {
     );
   }
 
+  private _formatDate(d: Date): string {
+    let month: any = d.getMonth() + 1;
+    month = month > 9 ? month: '0' + month;
+    return `${d.getFullYear()}-${month}-${d.getDate()}`;
+  }
+
   getTimeoffs = (): Promise<any> => {
     return new Promise((resolve, reject) => {
       let data = [];
-      return request.get(this._makeRequest('time_off/requests/'))
+      let [start, end] = [new Date(), new Date()];
+      start.setMonth(start.getMonth() - 1);
+      end.setFullYear(end.getFullYear() + 1);
+      return request.get(this._makeRequest('time_off/requests/', `start=${this._formatDate(start)}&end=${this._formatDate(end)}`))
         .on('data', chunk => {
           data.push(chunk);
         })
