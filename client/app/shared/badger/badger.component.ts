c0260a4c85199ab7fd81e608b12174fab440212b
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2019-08-31 22:20:27 +0400

    Adds missing reference

--- client/app/shared/badger/badger.component.ts
+++ client/app/shared/badger/badger.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 
 import { BadgeService } from '../../services/badge.service';
 import { ItemBadgeService } from '../../services/itemBadge.service';
