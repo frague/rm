acb718e905402a6b3102f44633e3ef47cc7fa0e6
Nick Bogdanov <nbogdanov@griddynamics.com>
2020-02-06 16:32:31 +0400

    Fixes adding comments to reports data

--- server/controllers/assignment.ts
+++ server/controllers/assignment.ts
@@ -26,7 +26,6 @@ const defaultColumns = [
   'canTravel',
   'status',
   'commentsCount',
-  'comments'
 ];
 
 const resourceColumns = [
@@ -45,7 +44,7 @@ const resourceColumns = [
   'english',
 ];
 
-const columnName = new RegExp(/^[a-z0-9.]+$/, 'i');
+const columnName = new RegExp(/^[a-z0-9_ .]+$/, 'i');
 
 // const billableStatuses = ['Billable', 'Booked', 'PTO Coverage', 'Funded'];
 const billableStatuses = ['Billable', 'PTO Coverage'];
@@ -176,12 +175,11 @@ export default class AssignmentCtrl extends BaseCtrl {
 
     console.log('Query:', JSON.stringify(query));
     console.log('Resource query:', JSON.stringify(resourceMatch));
-    console.log('Columns1:', group);
+    console.log('Columns1:', columns);
     console.log('Group1:', group);
 
     let now = new Date();
     now.setDate(this.shift + now.getDate());
-
 
     // Extended columns information
     group = group.reduce((result, column) => {
@@ -202,251 +200,217 @@ export default class AssignmentCtrl extends BaseCtrl {
     console.log('Project:', project);
 
     let cursor = Resource
-      .aggregate([
-        {
-          '$match': resourceMatch
+      .aggregate()
+      .match(resourceMatch)
+      .lookup({
+        from: 'comments',
+        localField: 'login',
+        foreignField: 'login',
+        as: 'comments'
+      })
+      .addFields({
+        commentsCount: {'$size': '$comments'}
+      })
+      .lookup({
+        from: 'assignments',
+        localField: 'login',
+        foreignField: 'resourceId',
+        as: 'assignment'
+      })
+      .unwind({
+        path: '$assignment',
+        preserveNullAndEmptyArrays: true
+      })
+      .lookup({
+        from: 'initiatives',
+        localField: 'assignment.initiativeId',
+        foreignField: '_id',
+        as: 'initiative'
+      })
+      .unwind({
+        path: '$initiative',
+        preserveNullAndEmptyArrays: true
+      })
+      .lookup({
+        from: 'demands',
+        let: {
+          login: '$login',
+          name: '$name'
         },
-        {
-          '$lookup': {
-            from: 'comments',
-            localField: 'login',
-            foreignField: 'login',
-            as: 'comments'
-          }
-        },
-        {
-          '$addFields': {
-            commentsCount: {'$size': '$comments'}
-          }
-        },
-        {
-          '$lookup': {
-            from: 'assignments',
-            localField: 'login',
-            foreignField: 'resourceId',
-            as: 'assignment'
-          }
-        },
-        {
-          '$unwind': {
-            path: '$assignment',
-            preserveNullAndEmptyArrays: true
-          }
-        },
-        {
-          '$lookup': {
-            from: 'initiatives',
-            localField: 'assignment.initiativeId',
-            foreignField: '_id',
-            as: 'initiative'
-          }
-        },
-        {
-          '$unwind': {
-            path: '$initiative',
-            preserveNullAndEmptyArrays: true
-          }
-        },
-        {
-          '$lookup': {
-            from: 'demands',
-            let: {
-              login: '$login',
-              name: '$name'
-            },
-            pipeline: [
-              {
-                '$match': {
-                  '$expr': {
-                    '$or': [
-                      {'$in': ['$$login', '$candidates']},
-                      {'$in': ['$$name', '$candidates']}
-                    ]
-                  }
-                }
+        pipeline: [
+          {
+            '$match': {
+              '$expr': {
+                '$or': [
+                  {'$in': ['$$login', '$candidates']},
+                  {'$in': ['$$name', '$candidates']}
+                ]
               }
-            ],
-            as: 'proposed'
+            }
           }
-        },
-        {
-          '$addFields': {
-            'onVacation': {
+        ],
+        as: 'proposed'
+      })
+      .addFields({
+        'onVacation': {
+          '$cond': {
+            if: {
+              '$and': [
+                {'$lte': ['$assignment.start', now]},
+                {'$gte': ['$assignment.end', now]},
+                {'$eq': ['$assignment.initiativeId', 'vacation']},
+              ]
+            },
+            then: '$assignment.end',
+            else: ''
+          }
+        }
+      })
+      .addFields({
+        'activeUsVisa': {
+          '$reduce': {
+            input: '$visas',
+            initialValue: null,
+            in: {
               '$cond': {
                 if: {
                   '$and': [
-                    {'$lte': ['$assignment.start', now]},
+                    {'$gt': ['$$this.isUs', 0]},
+                    {'$gt': ['$$this.isUs', '$$value.isUs']},
+                    {'$ne': ['$$this.till', null]},
+                    {'$gt': ['$$this.till', now]},
+                  ]
+                },
+                then: '$$this',
+                else: '$$value'
+              }
+            }
+          }
+        }
+      })
+      .addFields({
+        'assignment.account': '$initiative.account',
+        'assignment.initiative': '$initiative.name',
+        'assignment.billable': {
+          '$cond': {
+            if: {
+              '$and': [
+                {'$in': ['$assignment.billability', billableStatuses]},
+                {'$gt': ['$assignment.involvement', 0]}
+              ]
+            },
+            then: 'true',
+            else: 'false'
+          }
+        },
+        canTravel: {
+          '$cond': {
+            if: {
+              '$gt': ['$activeUsVisa.till', now]
+            },
+            then: 'true',
+            else: 'false'
+          }
+        },
+        status: {
+          '$arrayElemAt': [
+            {
+              '$filter': {
+                input: '$comments',
+                as: 'status',
+                cond: {
+                  '$eq': ['$$status.isStatus', true]
+                }
+              }
+            },
+            0
+          ]
+        }
+      })
+      .addFields({
+        'assignment.billableNow': {
+          '$cond': {
+            if: {
+              '$and': [
+                {'$eq': ['$assignment.billable', 'true']},
+                {'$lte': ['$assignment.start', now]},
+                {'$or': [
                     {'$gte': ['$assignment.end', now]},
-                    {'$eq': ['$assignment.initiativeId', 'vacation']},
+                    {'$eq': ['$assignment.end', null]}
                   ]
                 },
-                then: '$assignment.end',
-                else: ''
-              }
-            }
+                {'$gt': ['$assignment.involvement', 0]}
+              ]
+            },
+            then: 'true',
+            else: 'false'
+          }
+        }
+      })
+      .group(Object.assign(group, {
+        _id: '$_id',
+        assignments: { '$push': '$assignment' },
+        assignmentsSet: { '$max': '$assignment._id' },
+        name: { '$first': '$name' },
+        grade: { '$first': '$grade' },
+        minDate: {'$min': '$assignment.start'},
+        maxDate: {'$max': '$assignment.end'},
+        billable: {'$max': '$assignment.billableNow'},
+        canTravel: { '$max': '$canTravel' },
+        onVacation: { '$max': '$onVacation' },
+        login: { '$first': '$login' },
+        status: { '$first': '$status' },
+        commentsCount: { '$first': '$commentsCount' },
+        commentsTemp: {'$first': '$comments'},
+        proposed: {'$first': '$proposed.login'},
+      }))
+      .addFields({
+        'assignments': {
+          '$cond': {
+            if: '$assignmentsSet',
+            then: '$assignments',
+            else: []
           }
         },
-        {
-          '$addFields': {
-            'activeUsVisa': {
-              '$reduce': {
-                input: '$visas',
-                initialValue: null,
-                in: {
-                  '$cond': {
-                    if: {
-                      '$and': [
-                        {'$gt': ['$$this.isUs', 0]},
-                        {'$gt': ['$$this.isUs', '$$value.isUs']},
-                        {'$ne': ['$$this.till', null]},
-                        {'$gt': ['$$this.till', now]},
-                      ]
-                    },
-                    then: '$$this',
-                    else: '$$value'
-                  }
-                }
-              }
-            }
-          }
-        },
-        {
-          '$addFields': {
-            'assignment.account': '$initiative.account',
-            'assignment.initiative': '$initiative.name',
-            'assignment.billable': {
-              '$cond': {
-                if: {
-                  '$and': [
-                    {'$in': ['$assignment.billability', billableStatuses]},
-                    {'$gt': ['$assignment.involvement', 0]}
-                  ]
-                },
-                then: 'true',
-                else: 'false'
-              }
-            },
-            canTravel: {
-              '$cond': {
-                if: {
-                  '$gt': ['$activeUsVisa.till', now]
-                },
-                then: 'true',
-                else: 'false'
-              }
-            },
-            status: {
-              '$arrayElemAt': [
+        comments: {
+          '$arrayToObject': {
+            '$map': {
+              input: '$commentsTemp',
+              as: 'comment',
+              in: [
                 {
-                  '$filter': {
-                    input: '$comments',
-                    as: 'status',
-                    cond: {
-                      '$eq': ['$$status.isStatus', true]
-                    }
+                  '$cond': {
+                    if: '$$comment.source',
+                    then: '$$comment.source',
+                    else: '0',
                   }
                 },
-                0
+                '$$comment.text'
               ]
             }
           }
-        },
-        {
-          '$addFields': {
-            'assignment.billableNow': {
-              '$cond': {
-                if: {
-                  '$and': [
-                    {'$eq': ['$assignment.billable', 'true']},
-                    {'$lte': ['$assignment.start', now]},
-                    {'$or': [
-                        {'$gte': ['$assignment.end', now]},
-                        {'$eq': ['$assignment.end', null]}
-                      ]
-                    },
-                    {'$gt': ['$assignment.involvement', 0]}
-                  ]
-                },
-                then: 'true',
-                else: 'false'
-              }
-            }
-          }
-        },
-        {
-          '$group': Object.assign(group, {
-            _id: '$_id',
-            assignments: { '$push': '$assignment' },
-            assignmentsSet: { '$max': '$assignment._id' },
-            name: { '$first': '$name' },
-            grade: { '$first': '$grade' },
-            minDate: {'$min': '$assignment.start'},
-            maxDate: {'$max': '$assignment.end'},
-            billable: {'$max': '$assignment.billableNow'},
-            canTravel: { '$max': '$canTravel' },
-            onVacation: { '$max': '$onVacation' },
-            login: { '$first': '$login' },
-            status: { '$first': '$status' },
-            commentsCount: { '$first': '$commentsCount' },
-            commentsTemp: {'$first': '$comments'},
-            proposed: {'$first': '$proposed.login'},
-          })
-        },
-        {
-          '$addFields': {
-            'assignments': {
-              '$cond': {
-                if: '$assignmentsSet',
-                then: '$assignments',
-                else: []
-              }
-            },
-            comments: {
-              '$arrayToObject': {
-                '$map': {
-                  input: '$commentsTemp',
-                  as: 'comment',
-                  in: [
-                    {
-                      '$cond': {
-                        if: '$$comment.source',
-                        then: '$$comment.source',
-                        else: '0',
-                      }
-                    },
-                    '$$comment.text'
-                  ]
-                }
-              }
-            }
-          }
-        },
-        {
-          '$match': query
-        },
-        {
-          '$sort': this.order
-        },
-        {
-          '$project': Object.assign(project, {
-            _id: 1,
-            assignments: 1,
-            assignmentsSet: 1,
-            name: 1,
-            grade: 1,
-            starts: 1,
-            ends: 1,
-            minDate: 1,
-            maxDate: 1,
-            billable: 1,
-            canTravel: 1,
-            login: 1,
-            status: 1,
-            commentsCount: 1,
-            proposed: 1,
-          })
         }
-      ])
+      })
+      .match(query)
+      .sort(this.order)
+      .project(Object.assign(project, {
+        _id: 1,
+        assignments: 1,
+        assignmentsSet: 1,
+        name: 1,
+        grade: 1,
+        starts: 1,
+        ends: 1,
+        minDate: 1,
+        maxDate: 1,
+        billable: 1,
+        canTravel: 1,
+        login: 1,
+        status: 1,
+        commentsCount: 1,
+        proposed: 1,
+      }))
+      .exec()
       .then(data => {
         let message = skillsRequested ? `Skills matched: ${skillsRequested}` : '';
         if (data && skillsByUser) {
